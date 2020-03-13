431eb3ca27fa3f5ed9e9c4fcc468673954191b93
Charles Perkins <charlesap@gmail.com>
2020-03-13 16:37:18 -0700

    consolidate obfs.h and obfs_fs.h

--- obfs.h
+++ obfs.h
@@ -4,7 +4,94 @@
 
 #include <linux/fs.h>
 #include <linux/pagemap.h>
-#include "obfs_fs.h"
+#include <linux/types.h>
+#include <linux/magic.h>
+
+/*
+ * The obfs filesystem constants/structures
+ */
+
+/*
+ * Thanks to Kees J Bot for sending me the definitions of the new
+ * obfs filesystem (aka V2) with bigger inodes and 32-bit block
+ * pointers.
+ */
+
+#define OBFS_ROOT_INO 1
+
+/* Not the same as the bogus LINK_MAX in <linux/limits.h>. Oh well. */
+#define OBFS_LINK_MAX	250
+#define MINIX2_LINK_MAX	65530
+
+#define OBFS_I_MAP_SLOTS	8
+#define OBFS_Z_MAP_SLOTS	64
+#define OBFS_VALID_FS		0x0001		/* Clean fs. */
+#define OBFS_ERROR_FS		0x0002		/* fs has errors. */
+
+
+/*
+ * The new obfs inode has all the time entries, as well as
+ * long block numbers and a third indirect block (7+1+1+1
+ * instead of 7+1+1). Also, some previously 8-bit values are
+ * now 16-bit. The inode is now 64 bytes instead of 32.
+ */
+struct obfs2_inode {
+	__u16 i_mode;
+	__u16 i_nlinks;
+	__u16 i_uid;
+	__u16 i_gid;
+	__u32 i_size;
+	__u32 i_atime;
+	__u32 i_mtime;
+	__u32 i_ctime;
+	__u32 i_zone[10];
+};
+
+/*
+ * obfs super-block data on disk
+ */
+struct obfs_super_block {
+	__u16 s_ninodes;
+	__u16 s_nzones;
+	__u16 s_imap_blocks;
+	__u16 s_zmap_blocks;
+	__u16 s_firstdatazone;
+	__u16 s_log_zone_size;
+	__u32 s_max_size;
+	__u16 s_magic;
+	__u16 s_state;
+	__u32 s_zones;
+};
+
+/*
+ * V3 obfs super-block data on disk
+ */
+struct obfs3_super_block {
+	__u32 s_ninodes;
+	__u16 s_pad0;
+	__u16 s_imap_blocks;
+	__u16 s_zmap_blocks;
+	__u16 s_firstdatazone;
+	__u16 s_log_zone_size;
+	__u16 s_pad1;
+	__u32 s_max_size;
+	__u32 s_zones;
+	__u16 s_magic;
+	__u16 s_pad2;
+	__u16 s_blocksize;
+	__u8  s_disk_version;
+};
+
+struct obfs_dir_entry {
+	__u16 inode;
+	char name[0];
+};
+
+struct obfs3_dir_entry {
+	__u32 inode;
+	char name[0];
+};
+
 
 #define INODE_VERSION(inode)	obfs_sb(inode->i_sb)->s_version
 #define OBFS_V1		0x0001		/* original obfs fs */
